bf9180fcd4bb4b6949aec667917697098bb72990
Elvis <elvinas@elvisolutions.com>
2021-02-22 21:45:11 +0400

    Created button class

---
+++ src/com/codeacademy/Button.java
@@ -0,0 +1,25 @@
+package com.codeacademy;
+
+public class Button {
+    private Long id;
+    private String label;
+
+    public Button() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+}
--- src/com/codeacademy/Main.java
+++ src/com/codeacademy/Main.java
@@ -5,6 +5,10 @@ public class Main {
     public static void main(String[] args) {
         PlaceHolder placeHolder = new PlaceHolder("Kazkas");
 
+        Button button = new Button();
+        button.setId(1L);
+        button.setLabel("Click me!");
+
         System.out.println(placeHolder.getLabel());
     }
 }
