c154b110665ad8c1e43575551ba897e66852d7b1
Elvis <elvinas@elvisolutions.com>
2021-02-23 23:24:12 +0400

    Refactoring of lektuvas klase: extracted repetative code

--- src/com/codeacademy/lecture11/exception_example/Lektuvas.java
+++ src/com/codeacademy/lecture11/exception_example/Lektuvas.java
@@ -8,22 +8,28 @@ import java.util.Random;
 
 public class Lektuvas {
 
+    private static final int PROBABILITY = 5;
+
     public void isskleistiVaziuokle()  {
         try {
             vaziuokle();
             System.out.println("OK: vaziuokle sekmingai isskleista");
-        } catch (LaikinaKlaida e) {
-            System.out.println("ERROR: nepavyko isskleisti vaziuokles. " + "Priezastis: " + e.getPriezastis() + ". Svarbumas: laikina");
-        } catch (SvarbiKlaida e) {
-            System.out.println("ERROR: nepavyko isskleisti vaziuokles. " + e.getPriezastis() + "Svarbumas: svarbu");
-        }  catch (VaziuoklesIsskleidimoKlaida e) {
-            System.out.println("ERROR: nepavyko isskleisti vaziuokles. " + "Priezastis: " + e.getPriezastis() + ". Svarbumas: nezinoma");
+        } catch (LaikinaKlaida exception) {
+            System.out.println(gautiKlaidosTeksta(exception.getPriezastis(), Svarbumas.LAIKINA));
+        } catch (SvarbiKlaida exception) {
+            System.out.println(gautiKlaidosTeksta(exception.getPriezastis(), Svarbumas.SVARBU));
+        }  catch (VaziuoklesIsskleidimoKlaida exception) {
+            System.out.println(gautiKlaidosTeksta(exception.getPriezastis(), Svarbumas.NEZINOMA));
         }
     }
 
-    void vaziuokle() throws VaziuoklesIsskleidimoKlaida {
+    private String gautiKlaidosTeksta(String priezastis, Svarbumas svarbumas) {
+        return String.format("ERROR: nepavyko isskleisti vaziuokles. Priezastis: %s. Svarbumas: ", priezastis).concat(svarbumas.name());
+    }
+
+    private void vaziuokle() throws VaziuoklesIsskleidimoKlaida {
         Random rand = new Random();
-        int x = rand.nextInt(4);
+        int x = rand.nextInt(PROBABILITY);
         switch (x) {
             case 0: throw new SvarbiKlaida("Neatsidare durys");
             case 1: throw new SvarbiKlaida("Nenusileido ratas");
---
+++ src/com/codeacademy/lecture11/exception_example/Svarbumas.java
@@ -0,0 +1,7 @@
+package com.codeacademy.lecture11.exception_example;
+
+public enum Svarbumas {
+    LAIKINA,
+    SVARBU,
+    NEZINOMA
+}
