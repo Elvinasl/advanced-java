5e47962918f183aea5f8b9b861fcb7db755202eb
Pavel Vrublevskij | Addendum <pavel.vrublevskij@addendum.lt>
2021-03-07 20:02:31 +0200

    simple maven example added

---
+++ src/com/codeacademy/maven/mvn_example_1/src/main/java/eu/codeacademy/maven/App.java
@@ -0,0 +1,23 @@
+package com.codeacademy.maven.mvn_example_1.src.main.java.eu.codeacademy.maven;
+
+import eu.codeacademy.maven.service.ManoKlase;
+import eu.codeacademy.maven.service.ManoKlaseImpl;
+import eu.codeacademy.maven.service.ManoKlaseV2Impl;
+
+import java.io.IOException;
+
+public class App {
+
+    public static void main(String[] args) throws IOException {
+        System.out.printf("Hello! %s", "a");
+
+        // klientui a
+        ManoKlase manoKlase = new ManoKlaseImpl();
+        manoKlase.sumTwoDigits(1, 2);
+
+        // klientui
+        ManoKlase manoKlasev2 = new ManoKlaseV2Impl();
+        manoKlasev2.sumTwoDigits(1, 2);
+    }
+}
+
---
+++ src/com/codeacademy/maven/mvn_example_1/src/main/java/eu/codeacademy/maven/service/ManoKlase.java
@@ -0,0 +1,6 @@
+package com.codeacademy.maven.mvn_example_1.src.main.java.eu.codeacademy.maven.service;
+
+public interface ManoKlase {
+
+    int sumTwoDigits(int a, int b);
+}
